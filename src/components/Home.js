37285c2ac1e6bee56028eca90215af586f07e1df
Allahnana <Usmanallahnana@gmail.com>
2022-04-26 06:27:24 +0100

    ....

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,6 +16,7 @@ function Home() {
                   <Skills/>
                   <Experience/>
                   <Education/>
+                  <Skill/>
                   
                 </div>
             </div>
